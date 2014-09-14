272d99160a3c9de6c8c9c2919cd6ff3309d97a69
Turncoat Tony <the.green.machine@gmail.com>
2014-09-14 21:52:57 +0200

    Fixed extra curly brackets

--- utils/acGenSkins.rb
+++ utils/acGenSkins.rb
@@ -3,8 +3,8 @@
 # a go with it to see if I could generate a skins list a little easier than
 # what I was doing with C...
 # If you know Ruby and want to touch this up, feel free to do so
-# It sometimes adds extra }, in the json and you have to go through and remove the
-# last comma in each array and for the last entry in the file.
+# You have to go through and remove the last comma in each array and for
+# the last JSON object in the file.
 #
 # To use this file, place it in your assetto corsa content/car directory and run
 # ruby acGenSkins.rb from your command line(Need a Ruby aware cmd prompt)
@@ -15,24 +15,22 @@
 # http://jsonlint.com to make sure it cleaned it up properly
 ##
 
+$stdout.sync = true
 json = open("skins.json", "a")
 
 json.puts "["
 Dir.glob('*').each do|f|
 	if File.directory? f
 		json.puts(sprintf("%-4s{\n%-8s\"car\":\"%s\",\n", "", "", f))
-		puts f
 		json.puts(sprintf("%-8s\"skins\":[\n", ""))
 		Dir.chdir("#{f}/skins")
 		Dir.glob('*').each do|s|
 			if File.directory? s
 				json.puts(sprintf("%-12s\"#{s}\",", ""))
-				puts s
 			end
 		end
 		Dir.chdir("../../")
-		json.puts(sprintf("%-8s]\n", ""))
+		json.puts(sprintf("%-8s]\n%-4s},", "", ""))
 	end
-	json.puts(sprintf("%-4s},\n", ""))
 end
 json.puts "]"
