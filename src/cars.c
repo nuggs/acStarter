48ebfc5813b905256b87d9defc00fc1c4f0a6291
Anthony Goins <the.green.machine@gmail.com>
2021-09-20 12:07:53 -0700

    cars.c: Test if memory was allocated in alloc_car since we previously only initialized without checking if it returned NULL.
    Formatted code, not sure when I decided using eight spaced tabs was the
    cool thing to do.  This needs to be done to every file.  I don't trust
    indenting software to do this for me since I fail with them every time.
    It'll look better, some day.  I maybe promise.

--- src/cars.c
+++ src/cars.c
@@ -31,82 +31,86 @@
 LIST *skin_list;
 
 CAR_SKINS *alloc_skin(void) {
-	CAR_SKINS *skin;
-	int i = 0;
+    CAR_SKINS *skin;
+    int i = 0;
 
-	skin = malloc(sizeof(*skin));
-	skin->car = NULL;
-	for (i = 0; i < MAX_SKINS; i++)
-		skin->skin[i] = NULL;
-	return skin;
+    if (skin = malloc(sizeof(*skin)) == NULL) {
+        ac_log(ERROR, "alloc_skin(): Failed to allocate memory\n");
+        return NULL;
+    }
+
+    skin->car = NULL;
+    for (i = 0; i < MAX_SKINS; i++)
+        skin->skin[i] = NULL;
+    return skin;
 }
 
 void free_skin(CAR_SKINS *skin) {
-	int i = 0;
+    int i = 0;
 
-	detach_from_list(skin, skin_list);
-	free(skin->car);
-	for (i = 0; i < MAX_SKINS; i++) {
-		if (skin->skin[i] != NULL)
-			free(skin->skin[i]);
-	}
+    detach_from_list(skin, skin_list);
+    free(skin->car);
+    for (i = 0; i < MAX_SKINS; i++) {
+        if (skin->skin[i] != NULL)
+            free(skin->skin[i]);
+    }
 }
 
 int read_car_skins(const char *filename) {
-	JSON_Value *skin_root = NULL;
-	JSON_Array *skin_array, *skins_array = NULL;
-	JSON_Object *skin_object = NULL;
-	CAR_SKINS *skins = NULL;
-	int i, x, skin_count = 0;
+    JSON_Value *skin_root = NULL;
+    JSON_Array *skin_array, *skins_array = NULL;
+    JSON_Object *skin_object = NULL;
+    CAR_SKINS *skins = NULL;
+    int i, x, skin_count = 0;
 
-	skin_root = json_parse_file_with_comments(filename);
-	if (json_value_get_type(skin_root) != JSONArray) {
-		ac_log(ERROR, "JSON is not an array\n");
-		return -1;
-	}
+    skin_root = json_parse_file_with_comments(filename);
+    if (json_value_get_type(skin_root) != JSONArray) {
+        ac_log(ERROR, "JSON is not an array\n");
+        return -1;
+    }
 
-	fprintf(stdout, "Reading car skins: %s\n", filename);
-	skin_array = json_value_get_array(skin_root);
-        skin_count = json_array_get_count(skin_array);
-	for (i=0; i < skin_count; i++) {
-		skin_object = json_array_get_object(skin_array, i);
-		skins = alloc_skin();
-		skins->car = (json_object_get_string(skin_object, "car") != NULL) ? strdup(json_object_get_string(skin_object, "car")) : NULL;
-		skins_array = json_object_get_array(skin_object, "skins");
-		if (skins_array != NULL) {
-			for (x = 0; x < json_array_get_count(skins_array); x++) {
-				skins->skin[x] = (json_array_get_string(skins_array, x) != NULL) ? strdup(json_array_get_string(skins_array, x)) : NULL;
-				ac_log(SYSLOG, "Skin %s [%s] loaded.\n", skins->car, skins->skin[x]);
-			}
-		}
-		attach_to_list(skins, skin_list);
-	}
-	json_value_free(skin_root);
-	return 0;
+    fprintf(stdout, "Reading car skins: %s\n", filename);
+    skin_array = json_value_get_array(skin_root);
+    skin_count = json_array_get_count(skin_array);
+    for (i=0; i < skin_count; i++) {
+        skin_object = json_array_get_object(skin_array, i);
+        skins = alloc_skin();
+        skins->car = (json_object_get_string(skin_object, "car") != NULL) ? strdup(json_object_get_string(skin_object, "car")) : NULL;
+        skins_array = json_object_get_array(skin_object, "skins");
+        if (skins_array != NULL) {
+            for (x = 0; x < json_array_get_count(skins_array); x++) {
+                skins->skin[x] = (json_array_get_string(skins_array, x) != NULL) ? strdup(json_array_get_string(skins_array, x)) : NULL;
+                ac_log(SYSLOG, "Skin %s [%s] loaded.\n", skins->car, skins->skin[x]);
+            }
+        }
+        attach_to_list(skins, skin_list);
+    }
+    json_value_free(skin_root);
+    return 0;
 }
 
 const char *random_skin(const char *car) {
-	CAR_SKINS *skin = NULL;
-	ITERATOR iterator;
-	bool found = false;
-	int i = 0, max_skin = 0;
+    CAR_SKINS *skin = NULL;
+    ITERATOR iterator;
+    bool found = false;
+    int i = 0, max_skin = 0;
 
-	attach_iterator(&iterator, skin_list);
-	while ((skin = (CAR_SKINS *) next_in_list(&iterator)) != NULL) {
-		if (strcmp(car, skin->car) == 0) {
-			for (i = 0; i < MAX_SKINS; i++) {
-				if (skin->skin[i] != NULL) {
-					max_skin = i;
-				}
-			}
-			found = true;
-			break;
-		}
-	}
-	detach_iterator(&iterator);
+    attach_iterator(&iterator, skin_list);
+    while ((skin = (CAR_SKINS *) next_in_list(&iterator)) != NULL) {
+        if (strcmp(car, skin->car) == 0) {
+            for (i = 0; i < MAX_SKINS; i++) {
+                if (skin->skin[i] != NULL) {
+                    max_skin = i;
+                }
+            }
+            found = true;
+            break;
+        }
+    }
+    detach_iterator(&iterator);
 
-	if (found)
-		return skin->skin[random_range(0, max_skin)];
+    if (found)
+        return skin->skin[random_range(0, max_skin)];
 
-	return NULL;
+    return NULL;
 }
